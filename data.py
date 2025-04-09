89e3e421e4f9f00773c91a5a2abb5fd497f4faa8
uasim1702 <uasim1702@abv.bg>
2025-04-09 10:53:45 +0300

    users and topics

--- data.py
+++ data.py
@@ -3,6 +3,15 @@ from datetime import datetime
 
 class Users(BaseModel): # Uasim
     pass
+class Users(BaseModel):
+    id: int | None
+    first_name: str
+    last_name : str
+    telephone_number: int
+    email: str
+    is_admin: bool = False
+    password: str
+    date_registration: datetime = None
 
 class Messages(BaseModel): # Dimatur
     pass
@@ -14,8 +23,14 @@ class Categories(BaseModel): # Ivan
     date_create_access: datetime = Field(default_factory = datetime.now)
     user_id_user: int
 
-class Topics(BaseModel): # Uasim
-    pass
+class Topics(BaseModel):
+    id: int
+    title: str
+    user_id: int
+    category_id: int
+    is_locked: bool = None
+    created_at: datetime
+    best_reply_id: int
 
 class Replies(BaseModel): # Dimitur
     pass
