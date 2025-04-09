a62d4317a3f230f12d4ced394561bcdfa43db81e
uasim1702 <uasim1702@abv.bg>
2025-04-09 10:36:32 +0300

    Created data and main file

---
+++ data.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class Users(BaseModel): # Uasim
+    pass
+
+class Messages(BaseModel): # Dimatur
+    pass
+
+class Categories(BaseModel): # Ivan
+    pass
+
+class Topics(BaseModel): # Uasim
+    pass
+
+class Replies(BaseModel): # Dimitur
+    pass
+
+class ReplyVotes(BaseModel): # Ivan
+    pass
