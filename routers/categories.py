11c5ea136559f666c4b1b234e019196bd7590e60
uasim1702 <uasim1702@abv.bg>
2025-04-15 13:30:06 +0300

    update

--- routers/categories.py
+++ routers/categories.py
@@ -1,3 +1,12 @@
 from fastapi import APIRouter
 
 categories_router = APIRouter(prefix='/categories')
+
+
+@categories_router.get("/")
+def get_categories():
+    # ToDo authentication will follow
+    
+    categories = services.get_all_categories()
+    
+    
