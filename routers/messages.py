fd8379b4d14cbcbb40632e40648f89c1a6455c46
Flazer214 <ivanpustovit786@gmail.com>
2025-04-27 15:41:44 +0300

    work on messages

--- routers/messages.py
+++ routers/messages.py
@@ -1,5 +1,5 @@
 from data.models import Messages, MessagesCreate
-from services.messages_service import create_message
+from services.messages_service import create_message, view_conversation
 from fastapi import APIRouter
 from pydantic import BaseModel
 from routers.categories import categories_router
@@ -10,3 +10,7 @@ messages_router = APIRouter(prefix='/messages')
 @messages_router.post('/create',)
 def create(message: MessagesCreate):
     return create_message(message.sender_id, message.receiver_id, message.text)
+
+@messages_router.get('/conversation')
+def view(sender_id: int, receiver_id: int):
+    return view_conversation(sender_id, receiver_id)
--- services/messages_service.py
+++ services/messages_service.py
@@ -31,3 +31,25 @@ def create_message(sender_id, receiver_id, text):
         "sender_id": sender_id,
         "text": text
     }
+
+def view_conversation(sender_id, receiver_id):
+    view_con = read_query('SELECT * FROM messages WHERE sender_id = ? AND receiver_id = ? OR receiver_id = ? AND sender_id = ? ORDER BY created_at ASC', (sender_id, receiver_id, receiver_id, sender_id))
+    return [{
+        "receiver_id": t[0],
+        "sender_id": t[1],
+        "text": t[2],
+        "date": t[3],
+    }
+    for t in view_con
+    ]
+
+def view_conversations(receiver_id):
+    conversation = read_query('SELECT m.*, u.username FROM messages m JOIN users u ON m.sender_id = u.id JOIN users r ON m.receiver_id = r.id WHERE m.sender_id = ? OR m.receiver_id = ? ORDER BY m.created_at DESC', (receiver_id, receiver_id))
+    return [
+        {
+        'username': t[3],
+        'message': t[0],
+        'date': t[1]
+    }
+        for t in conversation
+    ]
