01acbd8ddad400bcad1d0e5d4b0dd559d6cfb5a4
Flazer214 <ivanpustovit786@gmail.com>
2025-04-29 22:33:22 +0300

    finish vote and get vote

--- routers/replies.py
+++ routers/replies.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
-from services.replies_service import create_replies
-from data.models import Replies, RepliesCreate
+from services.replies_service import create_replies, vote_replies, get_vote_reply
+from data.models import Replies, RepliesCreate, RepliesHasUsers
 replies_router = APIRouter(prefix='/replies')
 
 @replies_router.post('/create')
 def create(reply: RepliesCreate):
     return create_replies(reply.text, reply.user_id, reply.topic_id)
+
+@replies_router.post('/votes')
+def vote_reply(vote_reply: RepliesHasUsers):
+    return vote_replies(users_id = vote_reply.users_id, replies_id=vote_reply.replies_id, vote_type=vote_reply.vote_type)
+
+@replies_router.get('/replies/{replies_id}')
+def get_vote(replies_id: int):
+    return get_vote_reply(replies_id)
--- services/replies_service.py
+++ services/replies_service.py
@@ -34,17 +34,27 @@ def create_replies(text, user_id, topic_id):
         "topic_id": topic_id
     }
 
-def vote_replies():
-    vote_reply = read_query('SELECT user_id, reply_id, vote_type FROM replies_has_users WHERE user_id = ? AND reply_id = ?', (user_id, reply_id))
-    if vote_reply:
-        insert_query('UPDATE replies_has_users SET vote_type = ? WHERE user_id = ? AND reply_id = ?', (vote_type, user_id, reply_votes))
+def vote_replies(users_id, replies_id, vote_type):
+    is_exist_vote = read_query('SELECT vote_type FROM replies_has_votes WHERE users_id = ? AND replies_id = ?', (users_id, replies_id))
+    if is_exist_vote:
+        insert_query('UPDATE replies_has_votes SET vote_type = ? WHERE users_id = ? AND replies_id = ?',(vote_type, users_id, replies_id))
     else:
-        insert_query('INSERT INTO replies_has_users (user_id, reply_id, reply_type) VALUES (?, ?, ?)', (user_id, reply_id, vote_type))
+        insert_query('INSERT INTO replies_has_votes (users_id, replies_id, vote_type) VALUES (?, ?, ?)', (users_id, replies_id, vote_type))
     return {
-        "user_id": user_id,
-        "reply_id": reply_id,
+        "user_id": users_id,
+        "reply_id": replies_id,
         "vote_type": vote_type,
     }
 
-def best_replies():
-    NotImplemented()
+def get_vote_reply(replies_id: int):
+    result = read_query('SELECT (SELECT COUNT(*) FROM replies_has_votes WHERE replies_id = ? AND vote_type = 1) AS likes, (SELECT COUNT(*) FROM replies_has_votes WHERE replies_id = ? AND vote_type = 0)', (replies_id, replies_id))
+    if result:
+        likes, dislikes = result[0]
+    else:
+        likes, dislikes = 0, 0
+
+    return {
+        "id": replies_id,
+        "likes": likes,
+        "dislikes": dislikes
+    }
