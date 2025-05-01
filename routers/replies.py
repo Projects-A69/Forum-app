393e6a2d85ef6abf0f552666f546276dbd310b10
Flazer214 <ivanpustovit786@gmail.com>
2025-05-01 15:59:09 +0300

    auth message and reply, small fix for them

--- routers/replies.py
+++ routers/replies.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter
+from common.auth import get_user_or_raise_401
+from fastapi import APIRouter, HTTPException, Header
 from services.replies_service import create_replies, vote_replies, get_vote_reply
 from data.models import Reply, ReplyCreate, RepliesHasUsers
-replies_router = APIRouter(prefix='/replies')
+replies_router = APIRouter(prefix='/replies', tags=['Replies'])
 
 @replies_router.post('/create')
-def create(reply: ReplyCreate):
+def create(reply: ReplyCreate, x_token: str = Header()):
+    user = get_user_or_raise_401(x_token)
     return create_replies(reply.text, reply.user_id, reply.topic_id)
 
 @replies_router.post('/votes')
-def vote_reply(vote_reply: RepliesHasUsers):
+def vote_reply(vote_reply: RepliesHasUsers, x_token: str = Header()):
+    user = get_user_or_raise_401(x_token)
     return vote_replies(users_id = vote_reply.users_id, replies_id=vote_reply.replies_id, vote_type=vote_reply.vote_type)
 
 @replies_router.get('/replies/{replies_id}')
