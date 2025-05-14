e70745fe3c1b32cf595e5732ff0e37efef6f8d66
uasim1702 <uasim1702@abv.bg>
2025-05-14 14:03:44 +0300

    fb

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from fastapi.staticfiles import StaticFiles
 from routers.api.categories import categories_router
 from routers.api.messages import messages_router
 from routers.api.replies import replies_router
-# from routers.api.topics import topics_router
+from routers.api.topics import topics_router
 from routers.api.users import users_router
 from routers.api.category_access import access_router
 
 from routers.web.home import home_router
-# from routers.web.topics import web_topics_router
+from routers.web.topics import web_topics_router
 from routers.web.users import web_users_router
 from routers.web.category_access import admin_router
 from routers.web.messages import web_messages_router
@@ -21,12 +21,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(categories_router)
 app.include_router(messages_router)
 app.include_router(replies_router)
-# app.include_router(topics_router)
+app.include_router(topics_router)
 app.include_router(users_router)
 app.include_router(access_router)
 
 app.include_router(home_router)
-# app.include_router(web_topics_router)
+app.include_router(web_topics_router)
 app.include_router(web_users_router)
 app.include_router(admin_router)
 app.include_router(web_messages_router)
