d48c55921bc2a2bc94c3502be05d2c5fc31e125d
Flazer214 <ivanpustovit786@gmail.com>
2025-05-16 11:59:03 +0300

    work on htmlchat

--- routers/web/messages.py
+++ routers/web/messages.py
@@ -14,31 +14,35 @@ web_messages_router = APIRouter(prefix="/messages", tags=["Web Messages"])
 
 @web_messages_router.get("/{receiver_id}")
 def show_chat(request: Request, receiver_id: int, access_token: str | None = Cookie(default=None)):
+    current_user = None
     try:
-        user = get_user_or_raise_401(access_token)
+        current_user = get_user_or_raise_401(request.cookies.get("access_token"))
     except HTTPException:
         return templates.TemplateResponse("messages.html", {"request": request, "error": "You must login first."})
-    conversation = view_conversations(user.id)
-    messages = view_get_conversation(user.id, receiver_id)
+    conversation = view_conversations(current_user.id)
+    messages = view_get_conversation(current_user.id, receiver_id)
     return templates.TemplateResponse("messages.html", {"request": request,
                                                         "messages": messages,
                                                         "conversation": conversation,
                                                         "receiver_id": receiver_id,
-                                                        "user_id": user.id})
+                                                        "user_id": current_user.id,
+                                                        "current_user": current_user})
 
 @web_messages_router.get("/")
 def view_conversations_home(request: Request, access_token: str | None = Cookie(default=None)):
+    current_user = None
     try:
-        user = get_user_or_raise_401(access_token)
+        current_user = get_user_or_raise_401(request.cookies.get("access_token"))
     except HTTPException:
         return templates.TemplateResponse("messages.html", {"request": request, "error": "You must login first."})
-    conversation = view_conversations(user.id)
+    conversation = view_conversations(current_user.id)
     return templates.TemplateResponse("messages.html",{
                         'request':request,
                         "conversations": conversation,
                         'messages':[],
                         'receiver_id': None,
-                        'user_id': user.id
+                        'user_id': current_user.id,
+                        "current_user": current_user
                         })
 
 @web_messages_router.post("/go")
@@ -53,8 +57,9 @@ def create_message_page(
         receiver_id: int = Form(),
         created_at: date = Form(),
         access_token: str | None = Cookie(default=None)):
+    current_user = None
     try:
-        user = get_user_or_raise_401(access_token)
+        current_user = get_user_or_raise_401(request.cookies.get("access_token"))
     except HTTPException:
         return templates.TemplateResponse("messages.html", {"request": request, "error": "You must login first."})
 
@@ -63,14 +68,15 @@ def create_message_page(
         receiver_id=receiver_id,
         text=text
     )
-    create_messages(sender_id= user.id, receiver_id= messages_data.receiver_id, text=messages_data.text )
+    create_messages(sender_id= current_user.id, receiver_id= messages_data.receiver_id, text=messages_data.text )
 
     return RedirectResponse(url=f"/messages/{receiver_id}", status_code=302)
 
 @web_messages_router.post("/find")
 def find_by_username_web(request: Request, username: str = Form() ,access_token: str | None = Cookie(default=None)):
+    current_user = None
     try:
-        user = get_user_or_raise_401(access_token)
+        current_user = get_user_or_raise_401(request.cookies.get("access_token"))
     except HTTPException:
         return templates.TemplateResponse("messages.html", {"request": request, "error": "You must login first."})
 
